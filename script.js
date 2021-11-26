0b9b3d1cdfc0160052b94e803ab1659c654811fd
peslx <90468331+peslx@users.noreply.github.com>
2021-11-26 22:45:14 +0900

    Выполнено усложненное задание второго урока

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "Use sctrict";
+
+// 1
+let num = 266219;
+
+// 2
+num = String(num).split("");
+
+let multiNum = 1;
+
+num.forEach((element) => {
+  multiNum *= element;
+});
+
+console.log(multiNum);
+
+// 3
+multiNum **= 3;
+
+// 4
+console.log(String(multiNum).slice(0, 2));
