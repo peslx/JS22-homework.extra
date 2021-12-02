bac239795b8536292a33c2bf16d9d011186baf4e
peslx <90468331+peslx@users.noreply.github.com>
2021-12-03 03:16:24 +0900

    Выполнено усложненное задание 5 урока

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "Use sctrict";
+const arr = ["234", "111", "2051", "437", "4124", "358", "7560"];
+
+arr.forEach((element) => {
+  if (element[0] === "2" || element[0] === "4") console.log(element);
+});
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i++) if (num % i === 0) return false;
+  return num > 1;
+};
+
+for (let num = 1; num <= 100; num++) {
+  if (isPrime(num)) console.log(`${num} Делители этого числа: 1 и ${num}`);
+}
