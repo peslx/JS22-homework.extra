fa0475a09ba694e6bcbe40c1cd30693d7b3fae47
peslx <90468331+peslx@users.noreply.github.com>
2021-12-05 22:07:37 +0900

    Выполнено усложненное задание 7 урока

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "Use sctrict";
+const bodyWrap = document.querySelector(".body-wrap");
+const createElem = (tag) => {
+  return document.createElement(tag);
+};
+const today = new Date();
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+week.forEach((d, i) => {
+  const weekDay = createElem("span");
+  weekDay.innerHTML = `${d}<br>`;
+  if (i === 0 || i === 6) {
+    weekDay.style.fontStyle = "italic";
+  }
+  if (today.getDay() === i) weekDay.style.fontWeight = "bold";
+  bodyWrap.append(weekDay);
+});
