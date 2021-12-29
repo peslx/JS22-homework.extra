f82e9e86b6e357ef53f78fd82209f18aa3619de8
peslx <90468331+peslx@users.noreply.github.com>
2021-12-30 00:41:10 +0900

    Выполнено усложненное задание #2 урока 19

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "Use sctrict";
+
+const play = document.querySelector(".play");
+const reset = document.querySelector(".reset");
+const shakes = document.querySelectorAll(".shaker");
+let isPlayed = false;
+let start;
+
+const toDeafault = () => {
+  isPlayed = false;
+  cancelAnimationFrame(start);
+  shakes.forEach((item) => {
+    item.style.transform = "";
+  });
+};
+
+const letItShake = () => {
+  start = requestAnimationFrame(letItShake);
+
+  shakes.forEach((item) => {
+    item.style.transform = `rotateZ(${
+      Math.floor(Math.random() * -3) + Math.floor(Math.random() * 6)
+    }deg)`;
+  });
+};
+
+const onOff = () => {
+  isPlayed = !isPlayed;
+  isPlayed ? letItShake() : cancelAnimationFrame(start);
+};
+
+play.addEventListener("click", onOff);
+reset.addEventListener("click", toDeafault);
